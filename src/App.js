b399a016ec2bf0947fd27aecc9ded317df783afc
pankaj kore <korep95@gmail.com>
2022-05-02 00:27:20 +0530

    [pankaj]created bridgelabz is cool page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className='body'>
+      <body>
+        <h1>BridgeLabz is Cool</h1>
+        <img src="logo.jpg" width="100px" ></img> 
+        <p>At BridgeLabz,We're a Techie community of</p>
+        <li>Technologists</li>
+        <li>Thinkers</li>
+        <li>Builders</li> 
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Working together to keep the Tech Employability of Engineers alive and accessible,so tech companies worldwide can get contributors and creators for technology solutions.We believe this act of human collboration accross an Employability platform is essential to individual growth and our collective feature.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+            To know about us, visit <a href="https://www.bridgelabz.com/">Bridgelabz</a>
+            to learn even more about our mission i.e. <strong>Employability to all.</strong>
+        </p>
+      </body>
+      
+      </div>
     </div>
   );
 }
